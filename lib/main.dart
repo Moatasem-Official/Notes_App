84eac52cb86f1c174c37bec0d47d416c9ae1c3b6
Moatasem <moatasemnagy3@gmail.com>
2025-08-03 22:34:23 +0300

    Register Adapter

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/data/models/note_model.dart';
 import 'package:note_app/presentation/screens/home_screen.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('notes_box');
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(DevicePreview(enabled: !kReleaseMode, builder: (context) => MyApp()));
 }
 
